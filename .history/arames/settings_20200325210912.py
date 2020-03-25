25ba0941af55f1e7013ddb727b01ed19340bef6d
anandhu-vasu <anandhu.vasu101@gmail.com>
2020-03-25 22:05:37 +0530

    database changed to sqlite

--- .history/arames/settings_20200325210912.py
+++ .history/arames/settings_20200325210912.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'arames.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE':'django.db.backends.sqlite3',
+    #'ENGINE': 'django.db.backends.mysql',
+    #'NAME': 'dietapp',
+    #'HOST': 'localhost',
+    #'PORT': '3306',
+    #'USER': 'dietapp',
+    #'PASSWORD': 'passme',
     }
 }
 
--- .history/arames/settings_20200325214650.py
+++ .history/arames/settings_20200325214650.py
@@ -79,6 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #'ENGINE': 'django.db.backends.mysql',
+    #'NAME': 'dietapp',
+    #'HOST': 'localhost',
+    #'PORT': '3306',
+    #'USER': 'dietapp',
+    #'PASSWORD': 'passme',
     }
 }
 
